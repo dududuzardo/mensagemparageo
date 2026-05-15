b01ce0ef737e7c0879b87f09aef395815fa57e71
dududuzardo <edu262007@gmail.com>
2026-05-15 16:39:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ st.markdown("""
 
 # 3. O SEU "BANCO DE DADOS" DE MENSAGENS
 mensagens_diarias = {
-    "2026-05-13": "oiiiiiiiiiiiiiiiiiiii",
-    "2026-05-14": "Hoje é um novo dia. Lembre-se de beber água e sorrir! 🌸",
-    "2026-05-15": "Você está indo muito bem! Que seu dia seja leve e tranquilo. 💖",
-    "2026-05-16": "Pode escrever textos curtos ou longos, o cartão branco vai se adaptar sozinho."
+    "2026-05-15": "Oiiiiiii, esssa é a primeira mensagem pra vc geovanna vasconcelos, ou melhor, amor da minha vida. Volte amanhã",
+    "2026-05-16": "nada por aqui hj princesa, volte amanhã",
+    "2026-05-17": "Hmmmmmmmmm, ainda não, volte AMANHÃ...",
+    "2026-05-18": "FELIZ NOSSO DIA, eu te amo ininitamente meu amor, e criaria mais infinitos sites pra dizer isso. BEIJOOOOS. já tô com sdd..."
 }
 
 # 4. INTERFACE DO SITE
